7020c11dcb4dbf0fe1d6ffe9a7dba5ac762563df
Abdullah-765 <arshadabdullah765@gmail.com>
2025-07-16 17:26:48 +0500

    slight bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function Navbar() {
     return (
-      <div className="mt-[40px] font-bold text-[18px] md:text-[22px] justify-self-center flex border-black border-3 w-[80%] flex-row justify-around py-[10px] h-fit text-black items-center bg-amber-400 shadow-custom">
+      <div className="mt-[40px] font-bold text-[16px] md:text-[22px] justify-self-center flex border-black border-3 w-[80%] flex-row justify-around py-[10px] h-fit text-black items-center bg-amber-400 shadow-custom">
         <Link href={"/"} className="text-[20px] md:text-[26px] font-bold">Abdullah</Link>
         <ul className="flex gap-x-[30px]">
             <li className="hover:underline"><a href="#about">About Me</a></li>
